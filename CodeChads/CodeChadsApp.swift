d70ffb40b6a8d1a0c16ec4d03dcc0db43020c5ea
Nitish <nitishsolanki888@gmail.com>
2022-05-25 14:21:16 +0530

    Initial Commit

---
+++ CodeChads/CodeChadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeChadsApp.swift
+//  CodeChads
+//
+//  Created by Nitish Solanki on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CodeChadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
